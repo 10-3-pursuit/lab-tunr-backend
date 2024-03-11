962ce940522bba8b9db19bd1951cfa0aa43fe763
juligarc91 <julissagarcia@pursuit.org>
2024-03-11 17:00:01 -0400

    added time column with values

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 \c songs_dev
 
-INSERT INTO songs (name, artist, album, is_favorite) VALUES
-('Get Lucky', 'Daft Punk ft. Pharrell Williams', 'Random Access Memories', true),
-('No More Drama', 'Mary J. Blige', 'No More Drama', false),
-('Shoop', 'Salt-N-Pepa', 'Very Necessary', true),
-('Give Life Back to Music', 'Daft Punk', 'Random Access Memories', false),
-('Satta Massagana', 'The Abyssinians', 'Satta Massagana', true);
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Get Lucky', 'Daft Punk ft. Pharrell Williams', 'Random Access Memories', '4:08', true),
+('No More Drama', 'Mary J. Blige', 'No More Drama', '4:28', false),
+('Shoop', 'Salt-N-Pepa', 'Very Necessary', '4:09', true),
+('Give Life Back to Music', 'Daft Punk', 'Random Access Memories', '4:35', false),
+('Satta Massagana', 'The Abyssinians', 'Satta Massagana', '3:25', true);
