3a3ee5c8de79fc76cdbc1a27441a4ee6ee888aa5
AnitaOwen <anitaowen@pursuit.org>
2024-03-11 14:59:37 -0400

    created seed and schema files

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+
+\c tuner_dev;
+
+INSERT INTO songs 
+(name, album, time, is_favorite) 
+VALUES
+('Genie in a Bottle', 'Christina Aguilera', '03:38', TRUE),
+('Waterfalls', 'CrazySexyCool', '04:39', TRUE),
+('I Want it That Way', 'Millennium', '03:33', FALSE),
+('I Will Always Love You', 'The Bodyguard Soundtrack', '04:31', TRUE),
+('2 Become 1', 'Spice', '04:05', TRUE);
