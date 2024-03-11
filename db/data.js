90b862df51a72f628a98ea0903bf6462f06506a0
juligarc91 <julissagarcia@pursuit.org>
2024-03-11 17:07:56 -0400

    added data.js for reference

---
+++ db/data.js
@@ -0,0 +1,43 @@
+// data should look like this in browser when typing http://localhost:3003/api/songs
+[
+    {
+    "id": 1,
+    "name": "Get Lucky",
+    "artist": "Daft Punk ft. Pharrell Williams",
+    "album": "Random Access Memories",
+    "time": "4:08",
+    "is_favorite": true
+    },
+    {
+    "id": 2,
+    "name": "No More Drama",
+    "artist": "Mary J. Blige",
+    "album": "No More Drama",
+    "time": "4:28",
+    "is_favorite": false
+    },
+    {
+    "id": 3,
+    "name": "Shoop",
+    "artist": "Salt-N-Pepa",
+    "album": "Very Necessary",
+    "time": "4:09",
+    "is_favorite": true
+    },
+    {
+    "id": 4,
+    "name": "Give Life Back to Music",
+    "artist": "Daft Punk",
+    "album": "Random Access Memories",
+    "time": "4:35",
+    "is_favorite": false
+    },
+    {
+    "id": 5,
+    "name": "Satta Massagana",
+    "artist": "The Abyssinians",
+    "album": "Satta Massagana",
+    "time": "3:25",
+    "is_favorite": true
+    }
+]
