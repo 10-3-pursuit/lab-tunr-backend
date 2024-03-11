684d884652ea500e611854bb0dbfcd1044f1286f
Isiah Arrufat <isiaharrufat@pursuit.org>
2024-03-11 16:29:58 -0400

    completed part one of lab

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const songController = require("./controllers/songController.js");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/songs", songController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to TUNER App");
+});
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.json({ error: "Page not found" });
+});
+// EXPORT
+module.exports = app;
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+
+\c songs;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
+('Cornfield Chase', 'Hans Zimmer', 'Interstellar(original motion picture soundtrack', '2:06', true),
+('Stay', 'Hans Zimmer', 'Interstellar(original motion picture soundtrack', '6:21', true),
+('You were bigger than life', 'Shook', 'Continuum', '6:25', true),
+('Cumulus', 'Shook', 'Cloud Symphonies', '4:22', true),
+('slowdown', 'YellowStraps', 'girl whats up', '2:03', true);
+
