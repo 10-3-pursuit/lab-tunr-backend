6276b197bd607195e8b5ba49af62227f4ac2458b
brenda-soto <bsoto@pursuit.org>
2024-03-11 15:30:30 -0400

    Part 1 of Tuner backend done, including bonus

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 
 const app = express();
 
+const songController = require("./controllers/songController");
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/songs", songController);
+
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Welcome to Bookmarks App");
+  res.send("Welcome to Tuner App");
 });
 
 // 404 PAGE
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, genre, song_duration, is_favorite)
+VALUES
+('Hey Ya!', 'OutKast', 'Hip Hop/R&B', '4:10', true),
+('Beautiful', 'Christina Aguilara', 'Pop', '3:59', false),
+('In Da Club', '50 Cent', 'Hip Hop/Rap', '3:13', true),
+('Complicated', 'Avril Lavigne', 'Pop Punk', '4:04', false),
+('Bye Bye Bye', 'NSYNC', 'Pop', '3:20', true);
