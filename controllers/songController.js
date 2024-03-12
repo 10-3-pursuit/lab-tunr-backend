53069936fd52969d143cf1d24533a2c649c51f0f
brenda-soto <bsoto@pursuit.org>
2024-03-12 12:52:12 -0400

    Part 2 complete

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js");
+const { getAllSongs, getSong, createSong } = require("../queries/songs.js");
 
 songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
@@ -8,6 +8,23 @@ songs.get("/", async (req, res) => {
     res.status(200).json(allSongs);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.json(song);
+  } else return res.status(404).json({ error: "Not found" });
+});
+
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
--- queries/songs.js
+++ queries/songs.js
@@ -9,4 +9,31 @@ const getAllSongs = async () => {
   }
 };
 
-module.exports = { getAllSongs };
+const getSong = async (id) => {
+  try {
+    const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+    return oneSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createSong = async ({
+  name,
+  artist,
+  genre,
+  song_duration,
+  is_favorite,
+}) => {
+  try {
+    const newSong = await db.one(
+      "INSERT INTO songs (name, artist, genre, song_duration, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [name, artist, genre, song_duration, is_favorite]
+    );
+    return newSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllSongs, getSong, createSong };
