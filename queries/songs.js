03ae9433c4611948a944cc0634624d494d88929c
Jennifer <jenniferpeterson@pursuit.org>
2024-03-12 12:28:18 -0400

    created a function for getting just one song and exported it to use in the controller file

--- queries/songs.js
+++ queries/songs.js
@@ -1,6 +1,7 @@
 // queries/bookmarks.js
 const db = require("../db/dbConfigure.js");
 
+// to get all songs
 const getAllSongs = async () => {
   try {
     const allSongs = await db.any("SELECT * FROM songs");
@@ -10,4 +11,14 @@ const getAllSongs = async () => {
   }
 };
 
-module.exports = { getAllSongs };
+// to get ONE song
+const getOneSong = async (id) => {
+  try {
+    const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+    return oneSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllSongs, getOneSong };
