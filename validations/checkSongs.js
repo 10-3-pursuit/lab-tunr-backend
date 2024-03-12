0ea74f73d29e7efc3d37f434999f323524f4a4f6
Jennifer <jenniferpeterson@pursuit.org>
2024-03-12 13:17:39 -0400

    created a fx to validate the the album and time input. Must test it in postman to ensure it works correctly

--- validations/checkSongs.js
+++ validations/checkSongs.js
@@ -9,6 +9,20 @@ const checkNameAndArtist = (req, res, next) => {
   }
 };
 
+//fx to validate the length of album title
+const checkAlbum = (req, res, next) => {
+  if (req.body.album.length <= 2) {
+    res.status(400).json({ error: "Album title must be 3 characters or more" });
+  } else {
+    return next();
+  }
+};
+
+// fx to validate the length and data type of time
+const checkTime = (req, res, next) => {
+  console.log(req.body.time);
+};
+
 // fx to validate the is_favorite entry (must be a BOOLEAN)
 const checkBoolean = (req, res, next) => {
   const { is_favorite } = req.body;
@@ -25,4 +39,4 @@ const checkBoolean = (req, res, next) => {
   }
 };
 
-module.exports = { checkNameAndArtist, checkBoolean };
+module.exports = { checkNameAndArtist, checkBoolean, checkAlbum, checkTime };
