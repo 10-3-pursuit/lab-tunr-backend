6236bb13ed4858eb52ced9c091be16c20a0513b9
Jennifer <jenniferpeterson@pursuit.org>
2024-03-12 12:34:24 -0400

    creatd the route to get just one song

--- controllers/songController.js
+++ controllers/songController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const songs = express.Router();
 
 // storing the router in an object called songs
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getOneSong } = require("../queries/songs");
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -13,4 +13,16 @@ songs.get("/", async (req, res) => {
     res.status(500).json({ error: "server error" });
   }
 });
+
+// get ONE song
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getOneSong(id);
+  if (song) {
+    res.json(song);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
 module.exports = songs;
