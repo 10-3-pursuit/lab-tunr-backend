f3b506fad57696818c423eacfa78165d27e51e8c
Jennifer <jenniferpeterson@pursuit.org>
2024-03-11 20:10:28 -0400

    set up the route for getting all songs

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,5 +1,17 @@
 const express = require("express");
 
+// storing the router in an object called songs
+const { getAllSongs } = require("../queries/songs");
+
 const songs = express.Router();
 
+// INDEX
+songs.get("/", async (req, res) => {
+  const allSongs = await getAllSongs();
+  if (allSongs[0]) {
+    res.status(200).json(allSongs);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
 module.exports = songs;
