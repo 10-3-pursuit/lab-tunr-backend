60afe551557f4a419d5b74c9e76487294dea6b67
Jennifer <jenniferpeterson@pursuit.org>
2024-03-12 12:56:23 -0400

    created a fx for validating the name and artist input

---
+++ validations/checkSongs.js
@@ -0,0 +1,11 @@
+const checkName = (req, res, next) => {
+  if (!req.body.name) {
+    res.status(400).json({ error: "Name is required" });
+  } else if (!req.body.artist) {
+    res.status(400).json({ error: "Artist is required" });
+  } else {
+    return next();
+  }
+};
+
+module.exports = { checkName };
