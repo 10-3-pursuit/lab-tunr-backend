ab69785679905fec4e1608991cab87110a6b81fc
Isiah Arrufat <isiaharrufat@pursuit.org>
2024-03-14 02:28:14 -0400

    completed third part of lab update and delete

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong, inputSong } = require('../queries/songs');
+const { getAllSongs, getSong, inputSong, deleteSong, updateSong } = require('../queries/songs');
 
 const { checkSongInputs, checkBoolean } = require('../validations/checkSongs');
 
@@ -29,8 +29,30 @@ songs.post('/', checkSongInputs, checkBoolean,  async (req, res) => {
     const song = await inputSong(req.body)
     res.json(song)
    } catch (error) {
-        res.status(400).json({ error: error })
+        res.status(400).json({ error })
     }
 })
+
+// UPDATE
+songs.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    if (id) {
+      const updatedSong = await updateSong(id, req.body);
+      res.status(200).json(updatedSong);
+    } else {
+      res.status(400).json({ error });
+    }
+  });
+
+
+songs.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedSong = await deleteSong(id);
+    if (deletedSong.id) {
+      res.status(200).json(deletedSong);
+    } else {
+      res.status(404).json("song not found");
+    }
+  });
 
 module.exports = songs
